11058bf9b054cdeaa31b292b671c113163bf0ea9
Apricity <101598622+JustApricity@users.noreply.github.com>
2023-03-01 11:20:50 -0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ const productController = require('../controllers/productController.js')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('products/profile/:id', productController.renderProfile);
+router.get('/products', productController.viewProducts);
+router.get('/products/profile/:id', productController.renderProfile);
+router.get('/products/edit/:id', productController.renderEditForm);
+router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/add', productController.addProduct);
+router.get('/products/delete/:id', productController.deleteProduct);
 module.exports = router;
